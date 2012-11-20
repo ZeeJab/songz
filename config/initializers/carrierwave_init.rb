9da5823eed3b0363193ae6407f456a0641a68d6d
hsichang <hsi.chang@gmail.com>
2012-11-20 09:44:24 -0500

    deleted files

--- config/initializers/carrierwave_init.rb
+++ config/initializers/carrierwave_init.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWSSEC'],       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'song-a-tron1'                     # required
+  config.fog_directory  = 'song-a-tron-chang'                     # required
   config.fog_public     = false                                   # optional, defaults to true
 
 end
